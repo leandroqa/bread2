29253a6b4f8dacdfe860e7fabda77b7573f581e2
Leandro Roberto <leroberto@gmail.com>
2020-05-29 00:22:17 -0300

    updating public app.js

--- public/js/app.js
+++ public/js/app.js
@@ -70010,8 +70010,8 @@ var ProductList = /*#__PURE__*/function (_Component) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/leandro.roberto/personalproj/bread2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/leandro.roberto/personalproj/bread2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/leandro/dev/bread2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/leandro/dev/bread2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
