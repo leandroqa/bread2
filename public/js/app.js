54a8fa2e6575912c84c52827079a9763d78cdee9
Leandro Roberto <leandro.roberto@dextra-sw.com>
2020-06-07 22:33:06 -0300

    adding jest to frontend test

--- public/js/app.js
+++ public/js/app.js
@@ -70687,8 +70687,8 @@ var ProductList = /*#__PURE__*/function (_Component) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/leandro/dev/bread2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/leandro/dev/bread2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/leandro.roberto/personalproj/bread2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/leandro.roberto/personalproj/bread2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/js/tests/ProductCard.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ProductCard from '../components/ProductCard'
+
+describe("Product test", () => {
+  test('snapshot', () => {
+    const tree = renderer.create(<ProductCard productName="test" productImage="test.jpg" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+});
