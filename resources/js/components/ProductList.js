d76d0a520735351d7b182ccfbf8f000706dee3c4
Leandro Roberto <leandro.roberto@dextra-sw.com>
2020-06-19 15:04:35 -0300

    Adding dynamic product search

--- resources/js/components/ProductList.js
+++ resources/js/components/ProductList.js
@@ -8,7 +8,9 @@ class ProductList extends Component {
     constructor() {
         super()
         this.state = {
-            products: []
+            products: [],
+            loading : false,
+            value : ''
         }
         this.handleSearch = this.handleSearch.bind(this)
     }
@@ -19,42 +21,49 @@ class ProductList extends Component {
                 products: response.data
             })
         })
+    }   
+
+    async handleSearch(e) {
+        this.search(e.target.value);
+        this.setState({ value: e.target.value });
+    };
+    
+    async search(val){
+        this.setState({ loading: true });
+        try {
+            const res = await axios.post('/api/produtos/buscar', { productName: val });
+            const products = await res.data;
+            this.setState({ products, loading: false });            
+        } catch (e) {
+            console.log(`😱 Axios request failed: ${e}`);
+        }
+    };
+    
+
+    get renderProducts() {
+        const { products } = this.state
+        let prods = <h1>Não há produtos disponíveis.</h1>;
+        if (this.state.products) {
+            prods = products.map(product => (
+                <ProductCard key={product.id} productName={product.name} productImage={`images/${product.pic}`} productPrice={product.price} />
+            ))
+        }
+        return prods;
     }
 
-    handleSearch (event) {
-        this.setState({
-            searchVal: event.target.value
-        })
-        //console.log(this.state.searchVal)
-    }
 
-    render() {
-        const {products} = this.state
-        const searchInputBox = <input type="text" className="form-control" placeholder="buscar produto" id="searchFor" value={this.state.seachVal} onChange={this.handleSearch}/>
-        if(products.length > 0)
-            return (
+    render() {        
+        const searchInputBox = <input type="text" className="form-control" placeholder="buscar produto" id="searchFor" value={this.state.value} onChange={this.handleSearch}/>
+        return (
                 <div className='container py-4'>
                     <div className="input-group mb-3">
                         {searchInputBox}
                     </div>
                     <div className='row justify-content-center'>
-                        { products.map(product => (
-                            <ProductCard key={product.id} productName={product.name} productImage={`images/${product.pic}`} productPrice={ product.price} />                        
-                        ))}
+                        {this.renderProducts}
                     </div>                
                 </div>
-            )
-        else
-            return (
-                <div className='container py-4'>
-                    <div className="input-group mb-3">
-                        {searchInputBox}
-                    </div>
-                    <div className='row justify-content-center'>
-                        Não há produtos disponíveis.
-                    </div>                
-                </div>
-            )                            
+        )                                    
     }
 }
 
